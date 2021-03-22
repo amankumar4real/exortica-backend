f2f47259b3b7412882dbc52ed6f496181270de45
amankumar4real <amankumar4real@gmail.com>
2021-03-23 01:02:30 +0530

    second

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
   const Joi = require("joi");
 const nodemailer = require("nodemailer");
+const cors = require("cors")
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 const arr = [
