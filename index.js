b2b64cb821ed6e56f790cc36c32d4f8b32bb6e11
amankumar4real <amankumar4real@gmail.com>
2021-03-23 12:34:27 +0530

    third

--- index.js
+++ index.js
@@ -1,66 +1,41 @@
 const express = require("express");
-  const Joi = require("joi");
+const Joi = require("joi");
 const nodemailer = require("nodemailer");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-const arr = [
-  { id: 1, name: "abcd" },
-  { id: 2, name: "xyz" },
-];
-
 app.get("/", (req, res) => {
-  res.send("this is the rootO");
+  res.send("this is the root");
 });
 
-app.get("/api/array", (req, res) => {
-  res.send(JSON.stringify([1, 2, 3, 4, 5]));
-});
+app.post("/api/send-mail", async (req, res) => {
+  // getting body of the post call
+  const userDetails = req.body;
 
-app.get("/api/student/:id", (req, res) => {
-  const data = arr.find((a) => {
-    return a.id == req.params.id;
-  });
-
-  console.log(data, req.params.id);
-
-  if (data) {
-    res.send("data is available");
-  } else {
-    res.status(400).send({ error: true, message: "Data not available!" });
-  }
-});
-
-app.post("/api/student", (req, res) => {
+  // doing type check
   const schema = Joi.object({
     name: Joi.string().min(3).required(),
+    "date-from": Joi.string().required(),
+    "date-to": Joi.string().required(),
+    "room-type": Joi.string().required(),
+    "room-requirements": Joi.string().required(),
+    adults: Joi.number().required(),
+    children: Joi.number().required(),
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
+    "special-requirements": Joi.string().required(),
   });
 
   const result = schema.validate(req.body);
 
   if (result.error) {
-    res.status(400).send(result.error.details[0].message);
+    res.status(400).send({error: true, message: result.error.details[0].message});
     return;
   }
-
-  const dya = req.body.name;
-
-  arr.push({ id: arr.length + 1, name: dya });
-
-  res.send({
-    error: false,
-    newData: arr,
-    message: "New data added to the arr!",
-  });
-});
-
-app.post("/api/send-mail", async (req, res) => {
-  
-  // getting body of the post call
-  const userDetails = req.body
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
@@ -82,40 +57,38 @@ app.post("/api/send-mail", async (req, res) => {
       text-align: left;">Fields</th>
       <th style="padding: 15px; border: 1px solid #ddd;
       text-align: left;">Info</th>
-    </tr>`
+    </tr>`;
 
+  // going through all the keys of the object and adding in new data in the table
   for (const field in userDetails) {
-    // console.log(field.split("-").join(" ").toUpperCase())
+    console.log(typeof userDetails[field]);
     myHTML += `<tr>
     <th style="padding: 15px; border: 1px solid #ddd;
     text-align: left;">${field.split("-").join(" ").toUpperCase()}</th>
     <th style="padding: 15px; border: 1px solid #ddd;
     text-align: left;">${userDetails[field]}</th>
-  </tr>`
+  </tr>`;
   }
 
-  myHTML += "</table>"
-  
-
+  myHTML += "</table>";
 
   const mailOptions = {
     from: "patliputrahotels.booking@gmail.com", // sender address
-    to: ["amankumar4real@gmail.com", "amankumar3j@gmail.com", "soumya15d@gmail.com"], // list of receivers
+    to: [
+      "amankumar4real@gmail.com",
+      "amankumar3j@gmail.com",
+      "soumya15d@gmail.com"
+    ], // list of receivers
     subject: "Hotel Reservation Form", // Subject line
     html: myHTML, // html body
   };
 
   transporter.sendMail(mailOptions, function (err, info) {
     if (err) res.status(500).send(err);
-    else res.send(info);
+    else res.send({ error: false, message: "message delivered", info });
   });
 });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("listening on port !" + (process.env.PORT || 3000));
 });
-
-// define('_EMAIL_TO', 'reservations@patliputraexotica.com');
-// define('_EMAIL_SUBJECT', 'Hotel Reservation Form');
-// define('_EMAIL_FROM', $_POST["email"]);
-// Hipf@07.
